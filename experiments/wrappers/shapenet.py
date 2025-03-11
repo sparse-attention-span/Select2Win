49780cdf8780f67dbe7e42fec770fe1dc0785045
Maksim Zhdanov <mzhdano@hipster-cn007.localdomain>
2025-03-11 22:20:29 +0100

    rff positional encoding for shapenet model

--- experiments/wrappers/shapenet.py
+++ experiments/wrappers/shapenet.py
@@ -2,9 +2,21 @@ import torch
 import torch.nn as nn
 
 
+class Positional_Encoder(nn.Module):
+    def __init__(self, num_features, dimensionality=3, sigma=1.):
+        super().__init__()
+        self.linear = nn.Linear(dimensionality, num_features // 2, bias=False)
+        nn.init.normal_(self.linear.weight, 0.0, 1.0 / sigma)
+        
+    def forward(self, x):
+        proj = self.linear(x)
+        return torch.cat([torch.cos(proj), torch.sin(proj)], dim=-1)
+
+
 class ShapenetCarModel(nn.Module):
     def __init__(self, main_model):
         super().__init__()
+        self.pos_enc = Positional_Encoder(main_model.in_dim)
         self.main_model = main_model
         self.pred_head = nn.Sequential(
             nn.Linear(main_model.out_dim, main_model.out_dim),
@@ -15,7 +27,8 @@ class ShapenetCarModel(nn.Module):
         self.y_std_denorm = 48.5743
 
     def forward(self, node_positions, **kwargs):
-        return self.pred_head(self.main_model(node_positions, node_positions, **kwargs))
+        node_features = self.pos_enc(node_positions)
+        return self.pred_head(self.main_model(node_features, node_positions, **kwargs))
 
     @torch.no_grad()
     def denorm_mse(self, pred, target):
