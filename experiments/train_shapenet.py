b62bd68b35dec64eedafbe6959d9c5132f9e30bd
JohnYao93 <yaojohn6@gmail.com>
2025-05-06 05:31:35 +0000

    format train file

--- experiments/train_shapenet.py
+++ experiments/train_shapenet.py
@@ -1,8 +1,10 @@
 import sys
+
 sys.path.append("../../")
 
 import argparse
 import torch
+
 torch.set_float32_matmul_precision("high")
 from torch.utils.data import DataLoader
 from torch.optim import AdamW
@@ -18,24 +20,35 @@ import gc
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="erwin", 
-                        choices=('mpnn', 'pointtransformer', 'pointnetpp', 'erwin'))
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="erwin",
+        choices=("mpnn", "pointtransformer", "pointnetpp", "erwin"),
+    )
     parser.add_argument("--data-path", type=str, default="../shapenet_car/preprocessed")
-    parser.add_argument("--size", type=str, default="small", 
-                        choices=('small', 'medium', 'large'))
+    parser.add_argument(
+        "--size", type=str, default="small", choices=("small", "medium", "large")
+    )
     parser.add_argument("--num-epochs", type=int, default=100000)
     parser.add_argument("--batch-size", type=int, default=2)
     parser.add_argument("--use-wandb", type=int, default=1)
     parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--val-every-iter", type=int, default=100, 
-                        help="Validation frequency")
-    parser.add_argument("--experiment", type=str, default="shapenet", 
-                        help="Experiment name in wandb")
+    parser.add_argument(
+        "--val-every-iter", type=int, default=100, help="Validation frequency"
+    )
+    parser.add_argument(
+        "--experiment", type=str, default="shapenet", help="Experiment name in wandb"
+    )
     parser.add_argument("--test", type=int, default=0)
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--knn", type=int, default=8)
-    parser.add_argument("--profile", action="store_true", help="Use minimal profile configuration for testing")
-    
+    parser.add_argument(
+        "--profile",
+        action="store_true",
+        help="Use minimal profile configuration for testing",
+    )
+
     return parser.parse_args()
 
 
@@ -43,9 +56,15 @@ erwin_configs = {
     "profile": {
         "c_in": 64,
         "c_hidden": 64,
-        "ball_sizes": [256,],
-        "enc_num_heads": [8,],
-        "enc_depths": [1,],
+        "ball_sizes": [
+            256,
+        ],
+        "enc_num_heads": [
+            8,
+        ],
+        "enc_depths": [
+            1,
+        ],
         "dec_num_heads": [],
         "dec_depths": [],
         "strides": [],
@@ -100,7 +119,7 @@ if __name__ == "__main__":
 
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
-    
+
     train_dataset = ShapenetCarDataset(
         data_path=args.data_path,
         split="train",
@@ -126,7 +145,7 @@ if __name__ == "__main__":
         collate_fn=train_dataset.collate_fn,
         num_workers=args.batch_size,
     )
-    
+
     valid_loader = DataLoader(
         valid_dataset,
         batch_size=args.batch_size,
@@ -134,7 +153,7 @@ if __name__ == "__main__":
         collate_fn=train_dataset.collate_fn,
         num_workers=args.batch_size,
     )
-    
+
     test_loader = DataLoader(
         test_dataset,
         batch_size=args.batch_size,
@@ -149,7 +168,7 @@ if __name__ == "__main__":
             model_config = erwin_configs["profile"]
     else:
         raise NotImplementedError(f"Unknown model: {args.model}")
-    
+
     main_model = model_cls[args.model](**model_config)
     model = ShapenetCarModel(main_model).cuda()
     model = torch.compile(model)
@@ -164,5 +183,14 @@ if __name__ == "__main__":
     if args.profile:
         gc.collect()
     # Run the training
-    fit(config, model, optimizer, scheduler, train_loader, valid_loader, test_loader, num_epochs, args.val_every_iter)
-    
+    fit(
+        config,
+        model,
+        optimizer,
+        scheduler,
+        train_loader,
+        valid_loader,
+        test_loader,
+        num_epochs,
+        args.val_every_iter,
+    )
