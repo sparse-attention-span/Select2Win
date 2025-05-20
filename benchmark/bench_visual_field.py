fix grad computation

benchmark/bench_visual_field.py
@@ -8,46 +8,38 @@ from models import ErwinTransformer
 
 
 def compute_specific_grads(
-    model: nn.Module, x: torch.Tensor, i: int
+    model: nn.Module, x: torch.Tensor, target_idx: int
 ) -> torch.Tensor:
     """
-    Computes Jacobian d(out_j)/dx_i of output of model with respect to input.
+    Computes Jacobian d(out_target)/dx_i of output of model with respect to input.
 
     Arguments:
-        model: nn.Module model mapping a (n, d) tensor to a (n, d') tensor
+        model: nn.Module model mapping a (n, d) tensor to a (n, d) tensor
         x: tensor of shape (n, d) with grads enabled
-        i: i-th datapoint for which the gradients will be computed
-        j: j-th output point as target fn. If set to None, computes for all outputs.
+        target: output of d(out_target)/dx_i
 
     Returns:
-        If j is specified:
-            (d, d') tensor containig the Jacobian d(out_j)/dx_i
-        Otherwise:
-            (n, d, d') tensor containing Jacobians d(out_j)/dx_i for each output point j
+            (n, d, d') tensor containing Jacobians d(out_target)/dx_i for each input point i
     """
+    assert x.requires_grad
+
     out = model(x)
     n, d = x.shape
     device = x.device
-    n_prime, d_prime = out.shape
+    jacobian = torch.zeros((n, d, d), device=device)
 
-    jacobian_shape = (n_prime, d_prime, d)
+    for k in range(d):
+        # compute jacobian d(out[target, k] / dx_i)
+        # this is to prevent using jacobian function of PyTorch, which
+        # computes unnecessarily many gradients.
+        if x.grad is not None:
+            x.grad.zero_()
 
-    jacobian = torch.zeros(jacobian_shape, device=device)
-    # output_iter = [j] if j_is_set else range(n_prime)
+        out[target_idx, k].backward(retain_graph=True)
 
-    for j_prime in range(n_prime):
-        for k in range(d_prime):
-            # compute jacobian by computing each row d(out[j', k]/dx_i)
-            # this is to prevent using jacobian function of PyTorch, which
-            # computes unnecessarily many gradients
-            if x.grad is not None:
-                x.grad.zero_()
-
-            out[j_prime, k].backward(retain_graph=True)
-            grad_i = x.grad[i].detach().clone()
-
-            print(f"j_prime: {j_prime}, k: {k}, grad: {grad_i}")
-            jacobian[j_prime, k, :] = grad_i
+        for j in range(n):
+            jacobian[j, k, :] = x.grad[j].detach()
+            print(f"j: {j}, k: {k}, grad: {jacobian[j, k, :]}")
 
     return jacobian
 

tests/test_sparse_ball_attention.py
@@ -7,11 +7,7 @@ from einops import rearrange
 
 sys.path.append("./")
 from models import ErwinTransformer
-from models.erwin import (
-    NSAMSA_triton,
-    BallMSA,
-    NSAMSA
-)
+from models.erwin import NSAMSA_triton, BallMSA, NSAMSA
 
 from benchmark.bench_visual_field import compute_specific_grads
 
@@ -107,6 +103,8 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     EPS = 1e-20
     feature_dim = 2
     pos_dim = 2
@@ -138,8 +136,8 @@ if __name__ == "__main__":
     )
     tree_idx, tree_mask = build_balltree(node_positions, batch_idx)
     assert tree_mask.all()
-    node_positions = node_positions[tree_idx].to(device='cuda:0')
-    node_features = node_features[tree_idx].to(device='cuda:0')
+    node_positions = node_positions[tree_idx].to(device)
+    node_features = node_features[tree_idx].to(device)
     node_features.requires_grad_(True)
 
     fig, axes = plt.subplots(
@@ -167,18 +165,20 @@ if __name__ == "__main__":
         "num_heads": num_heads,
         "dimensionality": pos_dim,  # for visualization
         "ball_size": num_samples,
-        "selection_ball_size": 16
+        "selection_ball_size": 16,
+        "topk": 3,
+        "masks": True,
     }
 
     configBall = {
         "dim": feature_dim,
         "num_heads": num_heads,
         "dimensionality": pos_dim,  # for visualization
-        "ball_size": num_samples
+        "ball_size": num_samples,
     }
 
     # Erwin
-    # model = BallMSA(**configBall).to(device='cuda:0')
+    # model = BallMSA(**configBall).to(device)
     # add_model_visual_field(
     #     lambda x: model(x, node_positions, batch_idx),
     #     node_features,
@@ -188,16 +188,16 @@ if __name__ == "__main__":
     # )
 
     # Erwin with NSA
-    model = NSAMSA_triton(**config).to(device='cuda:0')
-    add_model_visual_field(
-        lambda x: model(x, node_positions, 1),
-        node_features,
-        node_positions,
-        i,
-        axes[0],
-    )
+    # model = NSAMSA_triton(**config).to(device)
+    # add_model_visual_field(
+    #     lambda x: model(x, node_positions, 1),
+    #     node_features,
+    #     node_positions,
+    #     i,
+    #     axes[0],
+    # )
 
-    model = NSAMSA(**config).to(device='cuda:0')
+    model = NSAMSA(**config).to(device)
     add_model_visual_field(
         lambda x: model(x, node_positions, 1),
         node_features,
@@ -205,7 +205,6 @@ if __name__ == "__main__":
         i,
         axes[1],
     )
-
 
     axes[0].set_title("Receptive field of triton")
     axes[1].set_title("Receptive field of pytorch")